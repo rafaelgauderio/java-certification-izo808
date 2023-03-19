bfed3cc4edfe9f640d89620e99bd8074f3dc3642
Rafael De Luca <deluca1712@gmail.com>
2023-03-19 16:40:25 -0300

    Create projected

---
+++ basics/ClassStructure.java
@@ -0,0 +1,8 @@
+package basics;
+
+public class ClassStructure {
+    public static void main(String[] args) {
+
+        System.out.println("Definition of the structure of a Java Class");
+    }
+}
