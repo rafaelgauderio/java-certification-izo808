9c80150c05549c7d72442b7cf263774c75cf4fdd
Rafael De Luca <deluca1712@gmail.com>
2023-03-25 21:16:32 -0300

    Features and Components of Java

---
+++ basics/Features.java
@@ -0,0 +1,20 @@
+package basics;
+
+public class Features {
+    public static void main(String[] args) {
+
+        System.out.println("Encapsulation\n" +
+                "\tOne of the pillar of Object Oriented Programming. Enable componentents to hide their data from other components.\n" +
+                "\tGive security to the software.\n" +
+                "Object Oriented\n" +
+                "\tOffers a natural way to thin about how the components of de software interact." +
+                "\tMake the code more organized in classes even if  the software grow int huge application with hige amount lines of code.\n" +
+                "Huge Library.\n" +
+                "\tJava has an huge library of well-tested and well-documented code.\n" +
+                "\tThis library helps us on every day developing.\n" +
+                "Memory Management\n" +
+                "\tJava provides automic memory management, that avoid memory leaks.\n" +
+                "\tJava provides that by the GARBAGE COLLECTOR.");
+    }
+
+}
