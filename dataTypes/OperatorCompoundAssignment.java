1e13413318de534d5927d5066b7fba2dc0a6fc50
Rafael De Luca <deluca1712@gmail.com>
2023-04-16 15:58:34 -0300

    Operators Compound Assigment

---
+++ dataTypes/OperatorCompoundAssignment.java
@@ -0,0 +1,24 @@
+package dataTypes;
+
+public class OperatorCompoundAssignment {
+
+    public static void main(String[] args) {
+
+        System.out.println("+=, -=, *= /=\n" +
+                "x+=y   equals x = x + y" +
+                "x-=y   equals x = x - y" +
+                "x*=y   equals x = x * y" +
+                "x/=y   equals x = x/y");
+
+        System.out.println("There is a in-bluid implicit cast");
+
+        int i =1;
+
+        i += (i=4) * (i + 3);
+        // i = (4) * (4 + 3 ) + 1;
+        // i = 28 + 1 = 29
+        System.out.println(i); //29
+
+
+    }
+}
