4b56f2e5e7a2b43dce21f62e16188eea703969f9
Rafael De Luca <deluca1712@gmail.com>
2023-04-21 21:53:47 -0300

    loop - for Each

--- controllingFlow/Loops.java
+++ controllingFlow/Loops.java
@@ -1,10 +1,16 @@
 package controllingFlow;
 
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
 public class Loops {
 
     public static void main(String[] args) {
 
         traditionalFor();
+        enhancedFor();
     }
 
     public static void traditionalFor () {
@@ -26,5 +32,29 @@ public class Loops {
             System.out.println(x + " " + y);
         }
         System.out.println("--------------------------");
+    }
+
+    public static void enhancedFor() {
+        System.out.println("For each iteration");
+
+        List<String> names = new ArrayList<String>();
+        names.add("Rafael");
+        names.add("Claudia");
+        names.add("Juliana");
+        names.add("Atena");
+
+        for(String nome : names) {
+            System.out.println(nome);
+        }
+        System.out.println("-----------------");
+
+        Set<Integer> numbers = new HashSet<Integer>();
+        for(int i=0 ; i<=15 ; i= i+3){
+            numbers.add(i);
+        }
+
+        for(var nickname : numbers) {
+            System.out.println(nickname);
+        }
     }
 }
