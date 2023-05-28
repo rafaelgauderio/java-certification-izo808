4b8f6f456d5ecfcf38f974fb66eaa1b62fc8c032
Rafael de Luca <deluca1712@gmail.com>
2023-05-27 22:48:05 -0300

    Array Out Of Bounds Exception

--- exceptions/ProgramRuntimeExceptions.java
+++ exceptions/ProgramRuntimeExceptions.java
@@ -6,8 +6,13 @@ public class ProgramRuntimeExceptions {
 
     public static void main(String[] args) {
 
-        int size = exception.length();
-        System.out.println(size); // Null Pointer Exception
+        //int size = exception.length();
+        //System.out.println(size); // Null Pointer Exception
+
+        String [] names = {"Rafael", "Juliana"};
+        System.out.println(names[0]);
+        System.out.println(names[1]);
+        System.out.println(names[2]); //Array Index Out Of Bounds Exception
 
     }
 }
