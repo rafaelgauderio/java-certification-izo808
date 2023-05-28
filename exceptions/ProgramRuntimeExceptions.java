4f610b2376e386506a1803802f5eef9980859cce
Rafael de Luca <deluca1712@gmail.com>
2023-05-28 17:33:03 -0300

    Number Format Exception

--- exceptions/ProgramRuntimeExceptions.java
+++ exceptions/ProgramRuntimeExceptions.java
@@ -2,17 +2,28 @@ package exceptions;
 
 public class ProgramRuntimeExceptions {
 
-    static  public String exception; // initializade as null by default
-
     public static void main(String[] args) {
 
+        String exception; // initializade as null by default
         //int size = exception.length();
         //System.out.println(size); // Null Pointer Exception
 
+        /*
         String [] names = {"Rafael", "Juliana"};
         System.out.println(names[0]);
         System.out.println(names[1]);
         System.out.println(names[2]); //Array Index Out Of Bounds Exception
+        */
+
+        try {
+            int number = Integer.parseInt("17");
+            System.out.println(number);
+            double number2 = Double.parseDouble("rafael"); // NumberFormatException
+            System.out.println(number2);
+
+        } catch (NumberFormatException error) {
+            error.printStackTrace();
+        }
 
     }
 }
