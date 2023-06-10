3f64dc58e6b7364f63fa91e86798c25fcb8480d4
Rafael de Luca <deluca1712@gmail.com>
2023-06-09 22:13:23 -0300

    Array Introduction

---
+++ arrays/ArrayTheory.java
@@ -0,0 +1,27 @@
+package arrays;
+
+public class ArrayTheory {
+
+    public static void main(String[] args) {
+        System.out.println("Arrays are objects that store multiple variables of the same type.\n" +
+                "Arrays can be primitives variables or object references.\n" +
+                "The array itself is always an OBJECT on th HEAP memory (even a array of primitives)\n" +
+                "Arrays of arrays is a multi-dimensional\n" +
+                "Include the size of the array only wen instantiate with new or declaring the elements\n" +
+                "The compiler (JVM) need to know how much space to allocate on the heap for the nem array.\n" +
+                "Multidimensinal arrays have to populated the size from array from the left to right");
+        int [] notas; // recommended this way
+        int grades [];
+        int [] notas2 = new int[10];
+
+        String [] names;
+        String nomes [];
+        Thread [] threads = new Thread[5];
+
+        String [] [] fisrtAndLastNames;
+        Double [] [] [] prices; // threeDimensionArray
+
+        Integer [] [] number = new Integer[4][];
+        Integer [] [] number2 = new Integer[5][10];
+    }
+}
