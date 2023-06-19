e65fcdb4b60221db5466ae6ca7d9b0d8c6d1ffc3
Rafael de Luca <deluca1712@gmail.com>
2023-06-19 10:21:28 -0300

    Array of object references declared as an interface

--- arrays/ArrayObjectsReferences.java
+++ arrays/ArrayObjectsReferences.java
@@ -2,6 +2,10 @@ package arrays;
 
 import java.util.Arrays;
 
+interface ExecutiveCar {
+    public abstract void speedUp ();
+}
+
 class Car {
     private String name;
     private String string;
@@ -21,15 +25,30 @@ class Car {
 }
 
 
-class Ford extends Car {
+class Ford extends Car implements ExecutiveCar{
     public Ford(String name) {
         super(name);    }
 
+    @Override
+    public void speedUp() {
+        System.out.println("Speeding up a ford car!");
+    }
 }
 
-class Fiat extends Car {
+class Fiat extends Car implements ExecutiveCar {
 
     public Fiat(String name) {
+        super(name);
+    }
+
+    @Override
+    public void speedUp() {
+        System.out.println("Speeding up a fiat car!");
+    }
+}
+
+class Volksmagen extends Car {
+    public Volksmagen(String name) {
         super(name);
     }
 }
@@ -43,15 +62,21 @@ class Fiat extends Car {
                     "If the declared type is a class, you can put references of the declared type in the array.\n" +
                     "If the array is declared as an interface type, the array elements can refer to any instance of\n" +
                     "\tany class that implements the declared interface.\n" +
+                    "Do not concernet with array elements but rather the REFERENCE TO THE ARRAY OBJECT itself.\n" +
+                    "An a double array reference can not be assignet to anything that is not an double array, not even an double value.\n" +
+                    "ARRAY IS A OBJECT, so an double array reference can not refet to an double primitive.\n" +
                     "");
 
             // array tendo referências como elementos. classes são sempre variáveis do tipo ponteiro
 
             Fiat uno = new Fiat("Uno Mile");
             Ford escort = new Ford ("Escort Guaruja");
-            Car [] cars = {uno, new Car("Ferrari"), new Fiat("Stilo"), new Ford("Mustang"), escort};
+            Car [] cars = {uno, new Car("Ferrari"), new Fiat("Stilo"), new Ford("Mustang"), escort, new Volksmagen("Gol Geração 4")};
 
-                System.out.println(Arrays.toString(cars));
+            System.out.println(Arrays.toString(cars));
+
+            Car [] cars2 = {new Volksmagen("Passat alemão"), new Fiat("Uno mile 2005")};
+            System.out.println(Arrays.toString(cars2));
 
         }
 
---
+++ out/production/java-certification-izo808/README.md
@@ -0,0 +1,1 @@
+## tutorial para certificação java OCA 1ZO-808
